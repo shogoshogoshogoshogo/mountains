956f13da72124e3d9e7ef1a5306f648a065b15bb
shogoshogoshogoshogo <shogo1887@yahoo.co.jp>
2024-11-11 20:54:22 +0900

    コントローラー追加　編集

--- app/controllers/mountains_controller.rb
+++ app/controllers/mountains_controller.rb
@@ -7,6 +7,18 @@ class MountainsController < ApplicationController
     @mountain = Mountain.new
   end
 
+  def create
+    Mountain.create(mountain_params)
+    redirect_to '/'
+  end
+
+  def destroy
+    mountain = Mountain.find(params[:id])
+    mountain.destroy
+    redirect_to root_path
+  end
+
+
   def show
     @mountain = Mountain.find(params[:id])
     @comment = Comment.new

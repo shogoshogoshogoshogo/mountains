7d371f7fad01f98690292dd9b39c51b5720235db
shogoshogoshogoshogo <shogo1887@yahoo.co.jp>
2024-11-13 18:29:21 +0900

    コントローラー修正

--- db/migrate/20241111111050_create_mountains.rb
+++ db/migrate/20241111111050_create_mountains.rb
@@ -1,8 +1,13 @@
 class CreateMountains < ActiveRecord::Migration[7.0]
   def change
-    create_table :mountains do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :mountain, null: false, foreign_key: true
+    create_table :mountaions do |t|
+      t.string :name, null: false          # 山の名前
+      t.integer :height                    # 山の標高
+      t.string :location                   # 山の所在地
+      t.date :date_climbed                 # 登山日
+      t.string :difficulty                 # 難易度
+      t.text :description                       # メモ
+      t.references :user, null: false, foreign_key: true 
       t.timestamps
     end
   end
--- db/migrate/20241111113429_create_comments.rb
+++ db/migrate/20241111113429_create_comments.rb
@@ -1,13 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.string :name, null: false          # 山の名前
-      t.integer :height                    # 山の標高
-      t.string :location                   # 山の所在地
-      t.date :date_climbed                 # 登山日
-      t.string :difficulty                 # 難易度
-      t.text :description                       # メモ
       t.references :user, null: false, foreign_key: true 
+      t.references :mountain, null: false, foreign_key: true 
       t.timestamps
     end
   end

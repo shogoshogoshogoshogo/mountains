300bf2624f3ec51910e14d99c78edc4a5baf248a
shogoshogoshogoshogo <shogo1887@yahoo.co.jp>
2024-11-18 21:22:54 +0900

    mountainモデル編集

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -1,5 +1,13 @@
 class Mountain < ApplicationRecord
-  validates :name, presence: true
+  # ActiveRecordのアソシエーション
   belongs_to :user
   has_many :comments
+
+  # ActiveHashとのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :difficulty
+
+  # バリデーション
+  validates :name, presence: true
+  validates :difficulty_id, presence: true, numericality: { other_than: 1, message: "を選択してください" } # id:1（'---'）は無効とする
 end

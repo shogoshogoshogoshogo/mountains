9b527258a1ab523d113fe4c7a00504f6243830ad
shogoshogoshogoshogo <shogo1887@yahoo.co.jp>
2024-11-19 12:49:44 +0900

    commentsマイグレーションファイル修正

--- db/migrate/20241115105034_create_comments.rb
+++ db/migrate/20241115105034_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :user ,null: false, foreign_key: true 
-      t.references :mountain, null: false, foreign_key: true 
-      t.text :tex
+      t.string :content              # コメントの内容
+      t.references :user, null: false, foreign_key: true # ユーザーとの関連
+      t.references :mountain, null: false, foreign_key: true # 山との関連
       t.timestamps
     end
   end

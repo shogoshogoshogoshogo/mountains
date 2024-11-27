f942c10f9cc130fb6e2309fd4e569506917c658f
shogoshogoshogoshogo <shogo1887@yahoo.co.jp>
2024-11-27 20:28:26 +0900

    書き換え

--- app/models/month.rb
+++ app/models/month.rb
@@ -1,5 +1,18 @@
 class Month < ActiveHash::Base
-  self.data = (1..12).map { |m| { id: m, name: m } } # 1月から12月
+  self.data = [
+    { id: 1, name: '1月' },
+    { id: 2, name: '2月' },
+    { id: 3, name: '3月' },
+    { id: 4, name: '4月' },
+    { id: 5, name: '5月' },
+    { id: 6, name: '6月' },
+    { id: 7, name: '7月' },
+    { id: 8, name: '8月' },
+    { id: 9, name: '9月' },
+    { id: 10, name: '10月' },
+    { id: 11, name: '11月' },
+    { id: 12, name: '12月' },
+  ] # 1月から12月
 
   include ActiveHash::Associations
   has_many :mountains

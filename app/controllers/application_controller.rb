14cfbdf1187fce2b9b9d912eebfaf8f4c1cd86fa
shogoshogoshogoshogo <shogo1887@yahoo.co.jp>
2024-12-02 22:43:33 +0900

    微修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,8 @@ class ApplicationController < ActionController::Base
       authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
     end
+    def after_sign_in_path_for(resource)
+      root_path # または index_path など、適切なパスを指定
+    end
   end
 end
